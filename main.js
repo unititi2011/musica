5df277665ce8869cd9ef1f2da7685fa3df6aa9bf
unititi2011 <107140140+unititi2011@users.noreply.github.com>
2023-04-17 14:53:29 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+ppd=0;
 ppe=0;
 pulsoesquerdox=0;
 pulsoesquerdoy=0;
@@ -26,8 +27,29 @@ image(video,0,0,600,500);
 fill("#f50202")  
 stroke("#f50202")
 circle(pulsoesquerdox,pulsoesquerdoy,20);
+if (ppd>0.2){
+    if(pulsodireitoy>0&& pulsodireitoy<=100){
+        som.rate(0.5);
+    document.getElementById("velocidade").innerHTML="velocidade = 0.5"
+    }
+    else if(pulsodireitoy >100&& pulsodireitoy <=200){
+        som.rate(1.0);
+        document.getElementById("velocidade").innerHTML="velocidade = 1.0"
+    }
+    else if (pulsodireitoy >200&& pulsodireitoy <=300) {
+        som.rate(1.5);
+        document.getElementById("velocidade").innerHTML="velocidade = 1.5"
+    }
+    else if (pulsodireitoy >300&& pulsodireitoy <=400) {
+        som.rate(2.0);
+        document.getElementById("velocidade").innerHTML="velocidade = 2.0"
+}
+else if (pulsodireitoy >400) {
+    som.rate(2.5);
+    document.getElementById("velocidade").innerHTML="velocidade = 2.5"
+}}
 if(ppe>0.2){
-    innumberpe=Number(pulsodireitoy);
+    innumberpe=Number(pulsoesquerdoy);
 removedecimais=floor(innumberpe);
 volume=removedecimais/500;
 document.getElementById("volume").innerHTML="Volume = "+volume;
@@ -44,6 +66,7 @@ if(results.length>0){
     console.log (results);
     ppe=results[0].pose.keypoints[9].score;
     pulsoesquerdox=results[0].pose.leftWrist.x;
+ppd=results[0].pose.keypoints[10].score;
     pulsoesquerdoy=results[0].pose.leftWrist.y;
     console.log(pulsoesquerdox);
     console.log(pulsoesquerdoy);
